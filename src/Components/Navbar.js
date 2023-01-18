525438d6f001c260a2095442308f06c476ab822e
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-01-18 16:11:13 +0700

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+        <header className="navbar">
+            <h3>Logo</h3>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Help</a>
+                <a href="#">Blog</a>
+                <button>
+                    
+                </button>
+            </nav>
+        </header>
+    );
+}
+ 
+export default Navbar;
